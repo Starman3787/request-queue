ebc17c23f13319c2d06bcab0a2b4c1c475e0fcca
Starman <30315137+Starman3787@users.noreply.github.com>
2021-08-07 17:58:03 +0100

    use map

--- index.js
+++ index.js
@@ -4,28 +4,30 @@ class QueueHandler extends EventsEmitter {
     constructor({ TTL = 60 } = {}) {
         super();
         this.TTL = TTL;
-        this.queues = {};
+        this.queues = new Map();
     }
 
     add(key = "null", value) {
-        if (!this.queues[key])
-            this.queues[key] = require("expire-array")(1000 * this.TTL);
-        if (this.queues[key]._db.length == 0)
+        if (!this.queues.get(key))
+            this.queues.set(key, require("expire-array")(1000 * this.TTL));
+        if (this.queues.get(key)._db.length == 0)
             this.emit("next", key, value);
         else
             this.queues[key].push(value);
     }
 
     completed(key = "null") {
-        this.queues[key].shift();
+        const queue = this.queues.get(key).shift();
+        this.queues.set(key, queue);
         if (!this.queues[key][0])
             return;
         this.emit("next", key, this.queues[key][0]);
     }
 
     retryLater(key = "null", next = true, pause = 0) {
-        const toRetry = this.queues[key].shift();
-        this.queues[key].push(toRetry);
+        const toRetry = this.queues.get(key).shift();
+        const queue = this.queues.get(key).push(toRetry);
+        this.queues.set(key, queue);
         if (next == true)
             this.next(key);
         else if (pause != 0)
@@ -33,9 +35,11 @@ class QueueHandler extends EventsEmitter {
     }
 
     next(key = "null") {
-        if (!this.queues[key][0])
+        if (!this.queues.get(key)[0])
             return;
-        this.emit("next", key, this.queues[key][0]);
+        this.emit("next", key, this.queues.get(key)[0]);
+        const queue = this.queues.get(key).shift();
+        this.queues.set(key, queue);
     }
 }
 
