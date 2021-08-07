7adc96e90f1d7cca7d1260d5e89ba3270718decf
Starman <30315137+Starman3787@users.noreply.github.com>
2021-08-07 01:26:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# request-queue
+ Simple event-based queue system.
---
+++ index.js
@@ -0,0 +1,40 @@
+const EventsEmitter = require("events");
+
+class QueueHandler extends EventsEmitter {
+    constructor({ TTL } = {}) {
+        this.TTL = TTL;
+        this.queues = {};
+    }
+
+    add(key, value) {
+        if (!queues[key])
+            queues[key] = require("expire-array")(1000 * this.TTL);
+        if (queues[key].length == 0) {
+            this.emit(key, value);
+        } else {
+            queues[key].push(value);
+        }
+    }
+
+    completed(key) {
+        queues[key].shift();
+        if (!queues[key][0])
+            return;
+        this.emit(key, queues[key][0]);
+    }
+
+    retryLater(key, next = true) {
+        const toRetry = queues[key].shift();
+        queues[key].push(toRetry);
+        if (next == true)
+            this.next(key);
+    }
+
+    next(key) {
+        if (!queues[key][0])
+            return;
+        this.emit(key, queues[key][0]);
+    }
+}
+
+module.exports = QueueHandler;
